1a7433be9f762707d7338efc6e5b1bfcdd29beac
rafalos <rsulkowski1@gmail.com>
2025-08-20 17:36:04 +0200

    feat: add shallow authorization and logout btn in navbar

--- src/middleware.ts
+++ src/middleware.ts
@@ -1,10 +1,16 @@
-import { NextResponse } from 'next/server';
-import type { NextRequest } from 'next/server';
+import { NextRequest, NextResponse } from 'next/server';
+import { getSessionCookie } from 'better-auth/cookies';
 
-export function middleware(request: NextRequest) {
-  return NextResponse.redirect(new URL('/auth', request.url));
+export async function middleware(request: NextRequest) {
+  const sessionCookie = getSessionCookie(request);
+
+  if (!sessionCookie) {
+    return NextResponse.redirect(new URL('/auth', request.url));
+  }
+
+  return NextResponse.next();
 }
 
 export const config = {
-  matcher: '/home/:path*',
+  matcher: ['/home'],
 };
