ba9f3bafaf127516bd2338cd034af7c6e1a8ba29
rafalos <rsulkowski1@gmail.com>
2025-08-25 18:12:56 +0200

    feat: improve styling of genre pills

--- src/app/home/genres/index.tsx
+++ src/app/home/genres/index.tsx
@@ -6,11 +6,15 @@ const Genres = async () => {
 
   console.log(genres);
   return (
-    <div className='flex flex-wrap gap-4'>
+    <div
+      className='flex lg:flex-wrap gap-4 overflow-auto mb-4 scrollbar-hidden after:bg-white 
+      after:left-0 after:h-10 after:w-10 after:absolute after:blur-md before:bg-white before:right-0 
+      before:h-10 before:w-10 before:absolute before:blur-md lg:after:hidden lg:before:hidden'
+    >
       {genres.map(({ id, name }) => (
         <div
           key={id}
-          className='bg-white shadow-md border-black p-1 px-2 rounded-md hover:bg-accent hover:cursor-pointer transition-colors'
+          className='bg-white shadow-md h-8 border-black p-1 px-4 rounded-md hover:bg-accent hover:cursor-pointer transition-colors'
         >
           {name}
         </div>
