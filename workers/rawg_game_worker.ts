75079f14819a7e8351784db749d5629e3239582e
rafalos <rsulkowski1@gmail.com>
2025-08-28 21:57:23 +0200

    feat: use simple express listener to keep worker alive

--- workers/rawg_game_worker.ts
+++ workers/rawg_game_worker.ts
@@ -3,6 +3,9 @@ import 'dotenv/config';
 import { Worker } from 'bullmq';
 import { connection } from '@/lib/server/queue';
 import { fetchGame } from '@/lib/server/fetchers';
+import express from 'express';
+
+const app = express();
 
 const worker = new Worker(
   'rawg_game_queue',
@@ -22,4 +25,12 @@ worker.on('completed', (job) => {
 
 worker.on('failed', (job, err) => {
   console.log(`${job?.id} has failed with ${err.message}`);
+});
+
+app.get('/keep-worker-alive', (req, res) => {
+  res.send('kept alive');
+});
+
+app.listen(process.env.WORKER_PORT, () => {
+  console.log(`Listetning`);
 });
